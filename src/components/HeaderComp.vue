bd083afb111fc9f084f2d6092f0e143baa8aa548
AndreaPanzeriDev <andpanz@libero.it>
2022-12-03 20:02:22 +0100

    dropdown selection

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -2,24 +2,89 @@
   <div id="header">
     <img src="../assets/img/logo.png" alt="logo" />
     <div class="sectionLink">
-      <li class="dropdonMenu"><a href="#">HOME</a></li>
-      <li class="dropdonMenu"><a href="#">PAGES</a></li>
-      <li class="dropdonMenu"><a href="#">PROGRAMS</a></li>
-      <li class="dropdonMenu"><a href="#">TICKETS</a></li>
-      <li class="dropdonMenu"><a href="#">SPEAKERS</a></li>
-      <li class="dropdonMenu"><a href="#">PAPERS</a></li>
-      <li class="dropdonMenu"><a href="#">BLOG</a></li>
-      <li class="dropdonMenu"><a href="#">SHORTCODES</a></li>
+      <li class="dropdonMenu">
+        <a href="#">HOME</a>
+        <ul>
+          <li class="dropdownSelection"><a href="#"></a></li>
+          <li class="dropdownSelection"><a href="#"></a></li>
+          <li class="dropdownSelection"><a href="#"></a></li>
+          <li class="dropdownSelection"><a href="#"></a></li>
+          <li class="dropdownSelection"><a href="#"></a></li>
+        </ul>
+      </li>
+      <li class="dropdonMenu">
+        <a href="#">PAGES</a>
+        <ul>
+          <li class="dropdownSelection"><a href="#"></a></li>
+          <li class="dropdownSelection"><a href="#"></a></li>
+          <li class="dropdownSelection"><a href="#"></a></li>
+          <li class="dropdownSelection"><a href="#"></a></li>
+          <li class="dropdownSelection"><a href="#"></a></li>
+        </ul>
+      </li>
+      <li class="dropdonMenu">
+        <a href="#">PROGRAMS</a>
+        <ul>
+          <li class="dropdownSelection"><a href="#"></a></li>
+          <li class="dropdownSelection"><a href="#"></a></li>
+          <li class="dropdownSelection"><a href="#"></a></li>
+          <li class="dropdownSelection"><a href="#"></a></li>
+          <li class="dropdownSelection"><a href="#"></a></li>
+        </ul>
+      </li>
+      <li class="dropdonMenu">
+        <a href="#">TICKETS</a>
+        <ul>
+          <li class="dropdownSelection"><a href="#"></a></li>
+          <li class="dropdownSelection"><a href="#"></a></li>
+          <li class="dropdownSelection"><a href="#"></a></li>
+          <li class="dropdownSelection"><a href="#"></a></li>
+          <li class="dropdownSelection"><a href="#"></a></li>
+        </ul>
+      </li>
+      <li class="dropdonMenu">
+        <a href="#">SPEAKERS</a>
+        <ul>
+          <li class="dropdownSelection"><a href="#"></a></li>
+          <li class="dropdownSelection"><a href="#"></a></li>
+          <li class="dropdownSelection"><a href="#"></a></li>
+          <li class="dropdownSelection"><a href="#"></a></li>
+          <li class="dropdownSelection"><a href="#"></a></li>
+        </ul>
+      </li>
+      <li class="dropdonMenu">
+        <a href="#">PAPERS</a>
+        <ul>
+          <li class="dropdownSelection"><a href="#"></a></li>
+          <li class="dropdownSelection"><a href="#"></a></li>
+          <li class="dropdownSelection"><a href="#"></a></li>
+          <li class="dropdownSelection"><a href="#"></a></li>
+          <li class="dropdownSelection"><a href="#"></a></li>
+        </ul>
+      </li>
+      <li class="dropdonMenu">
+        <a href="#">BLOG</a>
+        <ul>
+          <li class="dropdownSelection"><a href="#"></a></li>
+          <li class="dropdownSelection"><a href="#"></a></li>
+          <li class="dropdownSelection"><a href="#"></a></li>
+          <li class="dropdownSelection"><a href="#"></a></li>
+          <li class="dropdownSelection"><a href="#"></a></li>
+        </ul>
+      </li>
+      <li class="dropdonMenu">
+        <a href="#">SHORTCODES</a>
+        <ul>
+          <li class="dropdownSelection"><a href="#"></a></li>
+          <li class="dropdownSelection"><a href="#"></a></li>
+          <li class="dropdownSelection"><a href="#"></a></li>
+          <li class="dropdownSelection"><a href="#"></a></li>
+          <li class="dropdownSelection"><a href="#"></a></li>
+        </ul>
+      </li>
       <li class="dropdonMenu"><a href="#">|</a></li>
       <li class="dropdonMenu">
         <a href="#"><i class="fa-solid fa-magnifying-glass"></i></a>
-        <ul>
-            <li class="dropdownSelection">cvvrvrvrrr</li>
-            <li class="dropdownSelection">c</li>
-            <li class="dropdownSelection">c</li>
-            <li class="dropdownSelection">c</li>
-            <li class="dropdownSelection">c</li>
-        </ul>
       </li>
     </div>
   </div>
@@ -51,27 +116,26 @@ export default {};
   position: relative;
 }
 
-.dropdonMenu > ul{
-    display: none;
+.dropdonMenu > ul {
+  display: none;
 }
 
-.dropdonMenu:hover > ul{
-    display: block;
-    position: absolute;
-    right: 0;
-    top: 0;
-    width: 25px;
+.dropdonMenu:hover > ul {
+  display: block;
+  position: absolute;
+  right: 15px;
+  top: 15px;
+  width: 25px;
 }
 
-.dropdownSelection{
-    list-style: none;
-    background-color: black;
-    opacity: 0.6;
-    width: 160px;
-    color: white;
-    padding: 10px;
+.dropdownSelection {
+  list-style: none;
+  background-color: black;
+  opacity: 0.6;
+  width: 160px;
+  color: white;
+  padding: 10px;
 }
-
 
 /*******************
          A
@@ -88,8 +152,6 @@ a:hover {
   padding-bottom: 3px;
   border-bottom: 3px solid salmon;
 }
-
-
 
 /*******************
     SECTIONLINK
