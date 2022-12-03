c1596fc0f5140f9bba002bfd93a42a8fdca22deb
AndreaPanzeriDev <andpanz@libero.it>
2022-12-03 19:56:37 +0100

    dropdown menú

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,74 +1,101 @@
 <template>
   <div id="header">
-    <img src="../assets/img/logo.png" alt="logo">
+    <img src="../assets/img/logo.png" alt="logo" />
     <div class="sectionLink">
-        <li><a href="#">HOME</a></li>
-        <li><a href="#">PAGES</a></li>
-        <li><a href="#">PROGRAMS</a></li>
-        <li><a href="#">TICKETS</a></li>
-        <li><a href="#">SPEAKERS</a></li>
-        <li><a href="#">PAPERS</a></li>
-        <li><a href="#">BLOG</a></li>
-        <li><a href="#">SHORTCODES</a></li>
-        <li><a href="#">|</a></li>
-        <li><a href="#"><i class="fa-solid fa-magnifying-glass"></i></a></li>
-        
+      <li class="dropdonMenu"><a href="#">HOME</a></li>
+      <li class="dropdonMenu"><a href="#">PAGES</a></li>
+      <li class="dropdonMenu"><a href="#">PROGRAMS</a></li>
+      <li class="dropdonMenu"><a href="#">TICKETS</a></li>
+      <li class="dropdonMenu"><a href="#">SPEAKERS</a></li>
+      <li class="dropdonMenu"><a href="#">PAPERS</a></li>
+      <li class="dropdonMenu"><a href="#">BLOG</a></li>
+      <li class="dropdonMenu"><a href="#">SHORTCODES</a></li>
+      <li class="dropdonMenu"><a href="#">|</a></li>
+      <li class="dropdonMenu">
+        <a href="#"><i class="fa-solid fa-magnifying-glass"></i></a>
+        <ul>
+            <li class="dropdownSelection">cvvrvrvrrr</li>
+            <li class="dropdownSelection">c</li>
+            <li class="dropdownSelection">c</li>
+            <li class="dropdownSelection">c</li>
+            <li class="dropdownSelection">c</li>
+        </ul>
+      </li>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style scoped>
-
 /*******************
        HEADER
 *******************/
-#header{
-    padding: 25px;
-    width: 70%;
-    margin: auto;
-    display: flex;
-    justify-content: space-between;
+#header {
+  padding: 25px;
+  width: 70%;
+  margin: auto;
+  display: flex;
+  justify-content: space-between;
 }
 
 /*******************
-        LI
+     DROPDOWN
 *******************/
 
-li{
-    list-style: none;
-    margin: 0 10px;
+.dropdonMenu {
+  list-style: none;
+  margin: 0 10px;
+  position: relative;
 }
+
+.dropdonMenu > ul{
+    display: none;
+}
+
+.dropdonMenu:hover > ul{
+    display: block;
+    position: absolute;
+    right: 0;
+    top: 0;
+    width: 25px;
+}
+
+.dropdownSelection{
+    list-style: none;
+    background-color: black;
+    opacity: 0.6;
+    width: 160px;
+    color: white;
+    padding: 10px;
+}
+
 
 /*******************
          A
 *******************/
 
-a{
-    text-decoration: none;
-    color: #8e8e8e;
-    font-size: 13px;
-    font-weight: 900;
-    
+a {
+  text-decoration: none;
+  color: #8e8e8e;
+  font-size: 13px;
+  font-weight: 900;
 }
 
-a:hover{
-    padding-bottom: 3px;
-    border-bottom: 3px solid salmon;
+a:hover {
+  padding-bottom: 3px;
+  border-bottom: 3px solid salmon;
 }
+
+
 
 /*******************
     SECTIONLINK
 *******************/
 
-.sectionLink{
-    display: flex;
+.sectionLink {
+  display: flex;
 }
-
-
 </style>
