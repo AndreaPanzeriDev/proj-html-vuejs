28f875fb18aca8b23dcd85c39e697b311ea5a21e
AndreaPanzeriDev <andpanz@libero.it>
2022-12-03 18:23:35 +0100

    initialization

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <HeaderComp/>
+  </div>
+</template>
+
+<script>
+import HeaderComp from './components/HeaderComp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComp
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
