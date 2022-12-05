1b0cd98b97e5bed0c723a54825b0e8b6a0bcdf78
AndreaPanzeriDev <andpanz@libero.it>
2022-12-05 14:19:32 +0100

    questionComp attached

--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <EventComp/>
+    <QuestionComp/>
   </div>
 </template>
 
 <script>
 import EventComp from './EventComp.vue'
+import QuestionComp from './QuestionComp.vue'
 
 export default {
     components:{
-        EventComp
+        EventComp,
+        QuestionComp
     }
 }
 </script>
