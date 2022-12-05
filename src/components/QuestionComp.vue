c81afb096ff981762801084b6e0ff33d821adaae
AndreaPanzeriDev <andpanz@libero.it>
2022-12-05 14:19:37 +0100

    creation

---
+++ src/components/QuestionComp.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="questionComp">
+    <div class="split">
+        <div class="fs-5"><i class="fa-solid fa-trophy"></i></div>
+        <div></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.questionComp{
+    width: 70%;
+    margin: auto;
+}
+
+.split{
+    width: calc(100% / 3);
+}
+</style>
