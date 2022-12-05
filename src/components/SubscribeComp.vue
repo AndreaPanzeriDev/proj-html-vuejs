e37699fde4a926aef10bfdc9ba94ccfd7e4ff79e
AndreaPanzeriDev <andpanz@libero.it>
2022-12-05 18:33:28 +0100

    creation card

---
+++ src/components/SubscribeComp.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="subscribe">
+    <div class="d-flex blocks">
+      <div class="block">
+        <h2>326</h2>
+        <h6>giorni</h6>
+      </div>
+      <div class="block">
+        <h2>326</h2>
+        <h6>giorni</h6>
+      </div>
+      <div class="block">
+        <h2>326</h2>
+        <h6>giorni</h6>
+      </div>
+      <div class="block">
+        <h2>326</h2>
+        <h6>giorni</h6>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SubscribeComp",
+};
+</script>
+
+<style scoped>
+#subscribe {
+  background-image: url("../assets/img/coutdown-bg1.jpg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 100%;
+  margin-top: 50px;
+  height: 500px;
+  display: flex;
+  justify-content: center;
+}
+
+
+
+.block {
+  background-color: black;
+  opacity: 0.7;
+  padding: 2px 20px;
+  border-radius: 5px;
+  margin: auto 5px;
+  color: white;
+}
+</style>
