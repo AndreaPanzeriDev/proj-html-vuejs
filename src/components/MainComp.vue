1a97e4192b5ec55a9e8f83fd55fd28b6ee2772e1
AndreaPanzeriDev <andpanz@libero.it>
2022-12-05 18:33:22 +0100

    subscribecomp attached

--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -2,17 +2,20 @@
   <div>
     <EventComp/>
     <QuestionComp :arrayAboutUs="arrayAboutUs"/>
+    <SubscribeComp/>
   </div>
 </template>
 
 <script>
 import EventComp from './EventComp.vue'
 import QuestionComp from './QuestionComp.vue'
+import SubscribeComp from './SubscribeComp.vue'
 
 export default {
     components:{
         EventComp,
-        QuestionComp
+        QuestionComp,
+        SubscribeComp
     },
     props:{
       arrayAboutUs: Array
