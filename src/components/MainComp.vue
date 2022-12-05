029d098efff34cd51db170aa62a1c465ee32dd7d
AndreaPanzeriDev <andpanz@libero.it>
2022-12-05 11:06:10 +0100

    attached a new vue file

---
+++ src/components/MainComp.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <EventComp/>
+  </div>
+</template>
+
+<script>
+import EventComp from './EventComp.vue'
+
+export default {
+    components:{
+        EventComp
+    }
+}
+</script>
+
+<style>
+
+</style>
