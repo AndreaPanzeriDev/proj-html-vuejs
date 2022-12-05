9db1976d822987d2cb74fe6ae74fb229cf02c93b
AndreaPanzeriDev <andpanz@libero.it>
2022-12-05 12:33:12 +0100

    global class

--- src/App.vue
+++ src/App.vue
@@ -32,4 +32,39 @@ html{
   margin: 0;
   box-sizing: 0;
 }
+
+.d-flex {
+  display: flex;
+}
+
+.text-center{
+  text-align: center;
+}
+
+.justify-content-center{
+  justify-content: center;
+}
+
+.text-white{
+  color: white;
+}
+
+.fw-bold{
+  font-weight: bold;
+}
+
+.ms-5{
+  margin: 0 5px;
+}
+
+.mt-50{
+  margin-top: 50px;
+}
+
+.button{
+  padding: 10px 15px;
+  border: none;
+  border-radius: 5px;
+  background-color: #f7260c;
+}
 </style>
