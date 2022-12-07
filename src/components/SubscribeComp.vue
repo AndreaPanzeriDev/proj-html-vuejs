950e7647e735d58e391952b2824870face6171b5
AndreaPanzeriDev <andpanz@libero.it>
2022-12-07 11:36:56 +0100

    real timer

--- src/components/SubscribeComp.vue
+++ src/components/SubscribeComp.vue
@@ -3,20 +3,20 @@
     <div class="container m-auto">
       <div class="d-flex blocks">
         <div class="block">
-          <h1>326</h1>
-          <h6>giorni</h6>
+          <h1>{{displaydays}}</h1>
+          <h6>DAYS</h6>
         </div>
         <div class="block">
-          <h1>326</h1>
-          <h6>giorni</h6>
+          <h1>{{displayhours}}</h1>
+          <h6>HOURS</h6>
         </div>
         <div class="block">
-          <h1>326</h1>
-          <h6>giorni</h6>
+          <h1>{{displayminutes}}</h1>
+          <h6>MINUTES</h6>
         </div>
         <div class="block">
-          <h1>326</h1>
-          <h6>giorni</h6>
+          <h1>{{displayseconds}}</h1>
+          <h6>SECONDS</h6>
         </div>
       </div>
       <h1>SUBSCRIBE FOR MORE UPDATE</h1>
@@ -33,8 +33,27 @@ export default {
   name: "SubscribeComp",
   data(){
     return{
-      mail: ''
+      mail: '',
+      displaydays: 0,
+      displayhours: 0,
+      displayminutes: 0,
+      displayseconds: 0,
     }
+  },
+  computed:{
+    _seconds: () => 1000,
+    _minutes(){
+      return this._seconds * 60
+    },
+    _hours(){
+      return this._minutes * 60
+    },
+    _days(){
+      return this._hours * 24
+    }
+  },
+  mounted(){
+    this.remainingTime()
   },
   methods:{
     checkemail(){
@@ -45,6 +64,29 @@ export default {
       }else{
         alert('check your mailbox')
       }
+    },
+
+    remainingTime(){
+      const timer = setInterval(() => {
+        const now  = new Date();
+        const end = new Date(2023, 4, 23, 10, 10, 10, 10);
+        const distance = end.getTime() - now.getTime()
+
+        if(distance < 0){
+          clearInterval(timer);
+          return
+        }
+
+        const days = Math.floor((distance / this._days));
+        const hours = Math.floor((distance % this._days) / this._hours);
+        const minutes = Math.floor((distance % this._hours) / this._minutes);
+        const seconds = Math.floor((distance % this._minutes) / this._seconds);
+
+        this.displayseconds = seconds < 10 ? "0" + seconds : seconds;
+        this.displayminutes = minutes < 10 ? "0" + minutes : minutes;
+        this.displayhours = hours < 10 ? "0" + hours : hours;
+        this.displaydays = days < 10 ? "0" + days : days;
+      }, 1000 );
     }
   }
 };
